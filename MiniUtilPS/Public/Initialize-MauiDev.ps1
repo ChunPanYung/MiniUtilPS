f8cfe4d2cc2b4a3955b73c301450f66d3af945c5
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-11-21 18:12:52 -0500

    feat: add comments and param.

--- MiniUtilPS/Public/Initialize-MauiDev.ps1
+++ MiniUtilPS/Public/Initialize-MauiDev.ps1
@@ -7,9 +7,16 @@ function Initialize-MauiDev {
     .OUTPUTS
     none
     #>
+    [CmdletBinding()]
+    param(
+        [switch] $Android
+    )
+    # Install dotnet workload maui
     dotnet workload install maui
-    dotnet workload install maui-android
+    # Install VS Code extension
     ext install ms-dotnettools.dotnet-maui
+    # Install dotnet tool for maui checking
     dotnet tool install -g Redth.Net.Maui.Check
+
     Write-Output "DONE"
 }
